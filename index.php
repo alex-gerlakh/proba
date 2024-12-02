9d76e2306163e18cfa379b0d33da38f2c7e42bef
alex-gerlakh <oleksandr@ninesquares.studio>
2024-12-02 11:28:37 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<div class="sidebar-socials add-class"> <!-- Main Comment -->
+    <div class="socials-in">
+        <div class="socials-list class1 master-class">
+            <ul>
+                <li>List 1</li>
+                <li>List 2</li>
+                <li>List 3</li>
+                <li>List 4</li>
+                <li>List 5</li>
+                <li>List 6</li>
+                <li>List 7</li>
+                <li>List 8</li>
+                <li>List 9</li>
+            </ul>
+        </div>
+    </div>
+</div>
