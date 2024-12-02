11513d0c5452f758f8ef698cafb098cad1b940d6
alex-gerlakh <oleksandr@ninesquares.studio>
2024-12-02 15:54:31 +0200

    dassdas

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<div class="sidebar-socials add-class"> <!-- Main 23454 Comment -->
+<div class="sidebar-socials add-class"> <!-- Alex 23454 Comment -->
     <div class="socials-in">
         <div class="socials-list class1 master-class">
             <ul>
-                <li>List 1</li> <!-- Main dfsdsfdsf -->
+                <li>List 1</li> <!-- Alex dgfdfg dfsdsfdsf -->
                 <li>List 2</li>
                 <li>List 3</li>
-                <li>List 4</li> <!-- 454534534 -->
+                <li>List 4</li> <!-- 5656464564 -->
                 <li>List 5</li>
                 <li>List 6</li>
                 <li>List 7</li>
