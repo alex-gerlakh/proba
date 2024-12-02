c70cad7a7c7009961fa786ef3c68f4cfb641ada3
alex-gerlakh <oleksandr@ninesquares.studio>
2024-12-02 16:11:24 +0200

    fsfsdgdertetretgіввфі3453ddfaswdasasdada

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
-<div class="sidebar-socials add-class"> <!-- Alex Comment -->
+<div class="sidebar-socials add-class"> <!-- Main Comment -->
     <div class="socials-in">
         <div class="socials-list class1 master-class">
             <ul>
-                <li>List 1</li> <!-- Alex commit -->
+                <li>List 1</li> <!-- Main commit -->
                 <li>List 2</li>
                 <li>List 3</li>
                 <li>List 4</li>
-                <li>List 5</li>
+                <li>List 5</li> <!-- Main commit -->
                 <li>List 6</li>
                 <li>List 7</li>
-                <li>List 8</li>
+                <li>List 8</li> <!-- Main commit -->
                 <li>List 9</li>
             </ul>
         </div>
