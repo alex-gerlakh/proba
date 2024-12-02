d5d954054b051ab6acf371a3f96d4aaa70fcd0d4
alex-gerlakh <oleksandr@ninesquares.studio>
2024-12-02 15:24:42 +0200

    fsfsdgdertetretgіввфі3453

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<div class="sidebar-socials add-class"> <!-- Alex Alex 111 Comment -->
+<div class="sidebar-socials add-class"> <!-- Main Main 111 Comment -->
     <div class="socials-in">
         <div class="socials-list class1 master-class">
             <ul>
-                <li>List 1</li>
+                <li>List 1</li> <!-- MAIN -->
                 <li>List 2</li>
                 <li>List 3</li>
-                <li>List 4</li>
+                <li>List 4</li> <!-- MAIN -->
                 <li>List 5</li>
                 <li>List 6</li>
                 <li>List 7</li>
