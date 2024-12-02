6a87e0c7f5a0024013ee3b5f1f9b1cc183c4c7a8
alex-gerlakh <oleksandr@ninesquares.studio>
2024-12-02 16:20:36 +0200

    asdasdasdasdasdasdadas

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<div class="sidebar-socials add-class"> <!-- Main Main Comment -->
+<div class="sidebar-socials add-class"> <!-- Alex Alex eqweqwe Comment -->
     <div class="socials-in">
         <div class="socials-list class1 master-class">
             <ul>
-                <li>List 1</li> <!-- Main commit -->
-                <li>List 2</li> <!-- Main asdsdadas -->>
+                <li>List 1</li> <!-- Alex commit -->
+                <li>List 2</li> <!-- Alex asdsdadas -->>
                 <li>List 3</li>
-                <li>List 4</li>
+                <li>List 4</li> <!-- ALEX -->
                 <li>List 5</li>
                 <li>List 6</li>
                 <li>List 7</li>
