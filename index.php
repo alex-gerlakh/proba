3fa4c39344a680f9cd17c633ad1fb69e229120c2
alex-gerlakh <oleksandr@ninesquares.studio>
2024-12-02 16:30:09 +0200

    adasd3453dfgd

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
         <div class="socials-list class1 master-class">
             <ul>
                 <li>List 1</li>
+                <li>List 2</li>
                 <li>List 3</li>
                 <li>List 4</li>
                 <li>List 5</li>
