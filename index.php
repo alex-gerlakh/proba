1eaa1d7cd45c3d905c623645bd4c60399226e035
alex-gerlakh <oleksandr@ninesquares.studio>
2024-12-02 15:18:24 +0200

    fsfsdgdertetretgіввфі3453

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<div class="sidebar-socials add-class"> <!-- Main comment -->
-<div class="sidebar-socials add-class"> <!-- !!! Alex Alex Alex Alex Comment -->
+<div class="sidebar-socials add-class"> <!-- Main Main 111 Comment -->
     <div class="socials-in">
         <div class="socials-list class1 master-class">
             <ul>
