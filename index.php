1e0d72076c3ecc366ff8ad1c000d1489160e4b68
alex-gerlakh <oleksandr@ninesquares.studio>
2024-12-02 16:28:24 +0200

    adasd3453dfgd

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
-<div class="sidebar-socials add-class"> <!-- Alex Comment -->
+<div class="sidebar-socials add-class"> <!-- Main Comment -->
     <div class="socials-in">
         <div class="socials-list class1 master-class">
             <ul>
                 <li>List 1</li>
-                <li>List 2</li>
                 <li>List 3</li>
                 <li>List 4</li>
                 <li>List 5</li>
