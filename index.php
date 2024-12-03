03b27cb73ce6f80e96bd0cfeafc5cea5c3d141c5
alex-gerlakh <oleksandr@ninesquares.studio>
2024-12-03 14:54:22 +0200

    dsdad3453gfgddasadadzsfaddasasd

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
     <div class="socials-in">
         <div class="socials-list class1 master-class">
             <ul>
-                <li>List 1</li> <!-- Main Main -->
-                <li>List 1</li> <!-- Alex -->
+                <li>List 1</li> <!-- Main Main Alex -->
                 <li>List 2</li>
                 <li>List 3</li>
                 <li>List 4</li>
